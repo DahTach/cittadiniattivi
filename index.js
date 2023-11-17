c7c6f588d578d4d2e8a9d918931d1da8008dbad8
DahTach <francesco.tacinelli@gmail.com>
2023-11-17 08:26:12 +0100

    add: fixes and new chisiamo section

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 window.onload = () => {
   // COOKIES
 
@@ -34,4 +36,27 @@ window.onload = () => {
       menu.close();
     });
 
+  function chiSiamoList() {
+    var arrayVariable = ['Pier Paolo', 'Stefano', 'Nicola', 'Giovanni', 'Erasmo', 'Matteo', 'Giovanni', 'Adriano', 'Alessio', 'Fabio', 'Gloria', 'Lorella', 'Mauro', 'Nicola', 'Stefania', 'Alessandro', 'Barbara Chiara', 'Cristina', 'Diego', 'Elena', 'Erika', 'Ivana Mariangela', 'Mirco', 'Nadia', 'Paolo', 'Renata', 'Vittorio', 'Francesco', 'Aurora', 'Giulia', 'Emma', 'Sofia', 'Leonardo', 'Lorenzo', 'Mattia', 'Giuseppe', 'Federico', 'Simone', 'Daniele', 'Michele', 'Luigi', 'Ettore', 'Alessandro', 'Francesco', 'Giovanni', 'Andrea', 'Laura', 'Tommaso', 'Sofia', 'Gloria', 'Matteo', 'Giulia'];
+    var arrayLength = arrayVariable.length;
+    var temp;
+    
+    for (i = 0; i < arrayLength; i++) {
+      temp = document.createElement('div');
+      temp.className = 'results';
+      temp.innerHTML = `
+              <div class="relative flex items-center px-3 py-2 space-x-2 bg-white border rounded-lg shadow-sm hover:shadow-md">
+                <div class="flex-1 min-w-0">
+                  <span class="focus:outline-none">
+                    <span class="absolute inset-0" aria-hidden="true"></span>
+                    <p class="text-sm font-medium text-gray-600">${arrayVariable[i]}</p>
+                  </span>
+                </div>
+              </div>`;
+      document.getElementById('chi_siamo_people').appendChild(temp);
+    }
+  }
+
+chiSiamoList()
 };
+
